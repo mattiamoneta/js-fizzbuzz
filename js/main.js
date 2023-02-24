66bc4f79aca39c217961ba250d83c7ac5443ff25
Mattia Moneta <moneta.mattia@yahoo.com>
2023-02-24 15:22:23 +0100

    Final

--- js/main.js
+++ js/main.js
@@ -7,24 +7,46 @@
 
 */
 
+const ObjTilesCollection = document.querySelector('.tiles-collection');
+
+
 let i;
 
 for(i = 1; i <= 100; i++){
 
+    const ObjNewTile = document.createElement('div');
+
     if ((i % 3 == 0) && (i % 5 == 0)){
+
+        ObjNewTile.classList.add('tile','bg-red');
+        ObjNewTile.append('fizzbuzz');
+        ObjTilesCollection.append(ObjNewTile);
 
         console.log('FizzBuzz');
 
     } else if (i % 3 == 0) {
 
+        ObjNewTile.classList.add('tile','bg-green');
+        ObjNewTile.append('fizz');
+        ObjTilesCollection.append(ObjNewTile);
+
         console.log('Fizz');
 
-        
+
     } else if (i % 5 == 0) {
+
+        ObjNewTile.classList.add('tile','bg-yellow');
+        ObjNewTile.append('buzz');
+        ObjTilesCollection.append(ObjNewTile);
 
         console.log('Buzz');
 
     } else {
+
+        ObjNewTile.classList.add('tile','bg-blue');
+        ObjNewTile.append(i);
+        ObjTilesCollection.append(ObjNewTile);
+
         console.log(i);
     }
 
